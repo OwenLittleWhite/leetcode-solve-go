c25ac1742d331352fc0a2ba75f7fc07eac3bbcc4
Owen <253065903@qq.com>
2021-09-08 17:23:36 +0800

    first commit

---
+++ add-two-numbers.go
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+package leetcodesolvego
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) (head *ListNode) {
+	var tail *ListNode
+	carry := 0
+	for l1 != nil || l2 != nil {
+		temp1, temp2 := 0, 0
+		if l2 != nil {
+			temp2 = l2.Val
+			l2 = l2.Next
+		}
+		if l1 != nil {
+			temp1 = l1.Val
+			l1 = l1.Next
+		}
+		sum := temp1 + temp2 + carry
+		sum, carry = sum%10, sum/10
+		if head == nil {
+			head = &ListNode{Val: sum}
+			tail = head
+		} else {
+			tail.Next = &ListNode{Val: sum}
+			tail = tail.Next
+		}
+	}
+	if carry>0 {
+		tail.Next = &ListNode{Val: carry}
+	}
+	return
+}
